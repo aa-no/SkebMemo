22d0c3934569a36e17389cbc29206e3eaa073f63
Zhefei Tian <tianzhefei@whu.edu.cn>
2024-06-01 13:08:49 +0800

    Add MIT license

--- SkebMemo.js
+++ SkebMemo.js
@@ -7,6 +7,7 @@
 // @match        *://skeb.jp/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=skeb.jp
 // @grant        GM_registerMenuCommand
+// @license      MIT
 // ==/UserScript==
 
 (function () {
